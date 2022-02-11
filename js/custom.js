143a943ae782dad4ac7273aad05fe41ec0faebe0
biddut4 <mdbiddut970@gmail.com>
2022-02-11 18:45:57 +0600

    upload

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+let mobileBtn = document.querySelector('#mobile_bar');
+let mobileMenu = document.querySelector('.nav_Area');
+
+window.onscroll = () =>{
+    mobileBtn.classList.remove('fa-times');
+    mobileMenu.classList.remove('active');
+}
+
+
+mobileBtn.addEventListener('click', () => {
+    mobileBtn.classList.toggle('fa-times');
+    mobileMenu.classList.toggle('active');
+})
+
+
+/*owl carosel*/
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+/*wow js*/
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animate__animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
